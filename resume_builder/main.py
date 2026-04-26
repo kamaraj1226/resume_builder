beec276e0926cdb18b83174528c4907196e7e733
kamaraj1226 <kamaraj1226@gmail.com>
2026-04-26 21:49:53 +0530

    Fresh start

--- resume_builder/main.py
+++ resume_builder/main.py
@@ -1,32 +1,6 @@
-from resume_builder.agents.orchestrator import orchestrator_agent
-from resume_builder.stream.stream import stream
-from resume_builder.utils import StreamObj, get_user_input
-import asyncio
-import uuid
-
-
-async def async_main() -> None:
-    agent = orchestrator_agent()
-    stream_obj = StreamObj(agent=agent)
-    config = {
-        "configurable": {"thread_id": str(uuid.uuid4())}
-    }  # It is requried for human in the loop conversation
-    while True:
-        user_input = get_user_input()
-        if user_input.strip() == "/quit":
-            break
-
-        if not user_input.strip():
-            continue
-        print("Input received processing....")
-        print("AI: ", end="")
-        stream(user_input, config=config, **stream_obj.model_dump())
-        print()
-
-
 def main():
-    asyncio.run(async_main())
+    print("Hello from resume-building!")
 
 
 if __name__ == "__main__":
-    asyncio.run(async_main())
+    main()
